992f00f867368bf6102b17c88b529fabc6e95203
bige3032 <bige3032@163.com>
2019-03-17 13:04:41 +0800

    后台整合上传

--- src/main/java/com/accp/action/lzh/MemberActionL.java
+++ src/main/java/com/accp/action/lzh/MemberActionL.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import com.github.pagehelper.PageInfo;
 @RestController
 @RequestMapping("/c/lzh/member")
 public class MemberActionL {
-	@Resource
+	@Autowired
 	private MemberBizL biz;
 	
 	/*
